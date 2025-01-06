e0e6956e0395fa75037c785b493bc57329b22237
shepherd-001 <shepherdlegend001@gmail.com>
2025-01-06 11:48:25 +0100

    first commit

---
+++ src/main/java/com/shepherd/spring_boot_full_text_search/SpringBootFullTextSearchApplication.java
@@ -0,0 +1,13 @@
+package com.shepherd.spring_boot_full_text_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullTextSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFullTextSearchApplication.class, args);
+    }
+
+}
